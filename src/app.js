042cf0fd11d5e1a2fe29df2e9a2f233526c3d981
devmatheusramos <dev.matheuskeeven@gmail.com>
2026-09-16 21:04:38 -0300

    Serve o frontend estatico pelo Express

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const routes = require('./routes');
@@ -6,6 +7,7 @@ const swaggerSpec = require('./config/swagger');
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/api', routes);
 
